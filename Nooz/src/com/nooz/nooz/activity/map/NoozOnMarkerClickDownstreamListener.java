0ff3e5d9ab29ce402dd59ff727a2af72cab2c7cb
Robert Stein <rstein92@gmail.com>
2014-06-20 14:17:31 -0500

    Open story on bubble click if bubble is a single

--- Nooz/src/com/nooz/nooz/activity/map/NoozOnMarkerClickDownstreamListener.java
+++ Nooz/src/com/nooz/nooz/activity/map/NoozOnMarkerClickDownstreamListener.java
@@ -3,6 +3,8 @@ package com.nooz.nooz.activity.map;
 import android.content.Context;
 
 import com.google.android.gms.maps.model.Marker;
+import com.nooz.nooz.activity.article.ArticleLauncher;
+import com.nooz.nooz.model.Story;
 import com.twotoasters.clusterkraf.ClusterPoint;
 import com.twotoasters.clusterkraf.OnMarkerClickDownstreamListener;
 
@@ -17,6 +19,10 @@ public class NoozOnMarkerClickDownstreamListener implements OnMarkerClickDownstr
 	@Override
 	public boolean onMarkerClick(Marker marker, ClusterPoint clusterPoint) {
 		// TODO Auto-generated method stub
+		if (clusterPoint.size() == 1) {
+			Story s = (Story) clusterPoint.getPointAtOffset(0).getTag();
+			ArticleLauncher.openStory(mContext, s);
+		}
 		return false;
 	}
 
