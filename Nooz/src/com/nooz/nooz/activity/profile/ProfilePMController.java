194abd23d5d47842e6fee342a81c8b4dec05bffe
Robert Stein <rob@Roberts-MacBook-Pro.local>
2014-11-15 02:32:34 -0800

    Reorganizing

--- Nooz/src/com/nooz/nooz/activity/profile/ProfilePMController.java
+++
@@ -1,5 +0,0 @@
-package com.nooz.nooz.activity.profile;
-
-public class ProfilePMController {
-
-}
--- Nooz/src/com/nooz/nooz/util/BlobReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.nooz.nooz.util;
-
-public interface BlobReceiver {
-
-	public void getStoriesCallBack();
-	public void getStoryImageCallBack(int i);
-}
