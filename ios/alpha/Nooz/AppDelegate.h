f1b13d30d5f1e3d504aabb8c0322754575621425
Robert Stein <rob@Roberts-MacBook-Pro.local>
2014-11-15 02:44:29 -0800

    Adding empty iOS project for alpha

---
+++ ios/alpha/Nooz/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Nooz
+//
+//  Created by Robert Stein on 11/15/14.
+//  Copyright (c) 2014 22 North, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ios/alpha/Nooz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nooz
+//
+//  Created by Robert Stein on 11/15/14.
+//  Copyright (c) 2014 22 North, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
